a9b3931bd363af197407b926647926ecab6785e4
Vadim Storozhuk <vadozy@yahoo.com>
2019-02-25 23:23:28 -0500

    End of lecture 26

---
+++ Quizzes/promises_quizz_01/q2_3_4.js
@@ -0,0 +1,39 @@
+const fs = require("fs");
+const zlib = require("zlib");
+
+// Using node util.promisify
+const util = require("util");
+const gzip = util.promisify(zlib.gzip);
+
+
+/*
+function gzip(data) {
+  return new Promise((resolve, reject) => {
+    zlib.gzip(data, (error, result) => {
+      if (error) {
+        reject("Error in zlib.gzip");
+      } else {
+        resolve(result);
+      }
+    });
+  });
+}
+*/
+
+function readFile(filename, encoding) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filename, encoding, (err, data) => {
+      if (err) return reject(err);
+      resolve(data);
+    });
+  });
+}
+
+const file = "../../node/files/demofile.txt";
+//const file = "./blah.blah";
+
+readFile(file, "utf-8")
+  .then(data => gzip(data))
+  .then(data => console.log(data))
+  .catch(err => console.log("Caught: ", err));
+
