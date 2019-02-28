c2c88f167fc907ec2122b9dfd580743d4abf5b4c
Vadim Storozhuk <vadim.storozhuk@man.com>
2019-02-28 15:59:46 -0500

    End of Section 5, end of Lecture 31

---
+++ Quizzes/promises_quizz_01/q6.js
@@ -0,0 +1,35 @@
+let authenticated = false;
+
+function authenticate() {
+  console.log("Authenticating");
+  return new Promise(resolve => setTimeout(() => {
+    authenticated = true;
+    resolve({ status: 200 });
+  }, 500));
+}
+
+function publish() {
+  console.log("Publishing");
+  return new Promise(resolve => setTimeout(resolve, 1000, { status: authenticated ? 200 : 403 }));
+}
+
+function timeout(sleep) {
+  return new Promise((resolve, reject) => setTimeout(reject, sleep, "timeout"));
+}
+
+function publishChain() {
+  return publish()
+    .then(val => {
+      console.log("then1: [ publishChain ] ", val);
+      if (val.status !== 200) {
+        return authenticate().then(val => publish());
+      } else {
+        return val
+      }
+    })
+    .catch(err => console.log("Catch All [ publishChain ]: ", err));
+}
+
+Promise.race( [publishChain(), timeout(3000)])
+  .then(val => console.log("Published! [ race ] ", val))
+  .catch(err => console.log("Tiomed out! [ race ]: ", err));
